ddcbe5d197c67794c63a8b3106ac708a42e412d7
y6tdgs76hy-byte <y6tdgs76hy@privaterelay.appleid.com>
2026-06-01 00:49:55 -0400

    Refactor embed selection to use switch statement

--- src/interactions/selectMenus/panel/mainPanel.js
+++ src/interactions/selectMenus/panel/mainPanel.js
@@ -10,39 +10,41 @@ export default {
 
         let embed;
 
-        if (value === 'information') {
-            embed = new EmbedBuilder()
-                .setTitle('ℹ️ Server Information')
-                .setDescription('Server information goes here.')
-                .setColor('Blue');
-        }
+        switch (value) {
 
-        if (value === 'roblox_rules') {
-            embed = new EmbedBuilder()
-                .setTitle('📜 Roblox Rules')
-                .setDescription('Roblox rules go here.')
-                .setColor('Red');
-        }
+            case 'information':
+                embed = new EmbedBuilder()
+                    .setTitle('ℹ️ Server Information')
+                    .setDescription('Server information goes here.')
+                    .setColor('Blue');
+                break;
 
-        if (value === 'discord_rules') {
-            embed = new EmbedBuilder()
-                .setTitle('📜 Discord Rules')
-                .setDescription('Discord rules go here.')
-                .setColor('Red');
-        }
+            case 'roblox_rules':
+                embed = new EmbedBuilder()
+                    .setTitle('📜 Roblox Rules')
+                    .setDescription('Roblox rules go here.')
+                    .setColor('Red');
+                break;
 
-        if (value === 'shop_perks') {
-            embed = new EmbedBuilder()
-                .setTitle('🛒 Shop / Booster Perks')
-                .setDescription('Shop and booster perks go here.')
-                .setColor('Green');
-        }
+            case 'discord_rules':
+                embed = new EmbedBuilder()
+                    .setTitle('📜 Discord Rules')
+                    .setDescription('Discord rules go here.')
+                    .setColor('Red');
+                break;
 
-        if (!embed) {
-            return interaction.reply({
-                content: 'Invalid selection.',
-                ephemeral: true
-            });
+            case 'shop_perks':
+                embed = new EmbedBuilder()
+                    .setTitle('🛒 Shop / Booster Perks')
+                    .setDescription('Shop and booster perks go here.')
+                    .setColor('Green');
+                break;
+
+            default:
+                return interaction.reply({
+                    content: 'Invalid selection.',
+                    ephemeral: true
+                });
         }
 
         return interaction.reply({
